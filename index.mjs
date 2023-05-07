5d7b1be6fb99e8a92ff98ce7365072d1bbc2fdb2
Daniel Eke <endanke@gmail.com>
2023-05-07 11:21:25 +0300

    Remove display class overrides

--- index.mjs
+++ index.mjs
@@ -1,12 +1,10 @@
 import { replSend, replRawMode, ensureConnected, listFilesDevice } from 'monocle-node-cli';
 import util from 'util';
 import { exec } from 'child_process';
-import { promises as fs } from 'fs';
 
 const execAsync = util.promisify(exec);
 const enter = '\x1B[F\r';
 const lineCharLimit = 27;
-const displayAPI = await fs.readFile('wip_display.py', 'utf8');
 const lineLimit = 6;
 var lineBuffer = new Array(lineLimit);
 lineBuffer.fill(".");
@@ -38,12 +36,12 @@ async function sendLastLine() {
     lineBuffer.push(lastLine.slice(-lineCharLimit));
 
     var replCommmand = `import display;`;
-    replCommmand += `t1 = display.Text("${stringWithPlaceholder(lineBuffer[5])}", 10, 10, display.GREEN);`;
-    replCommmand += `t2 = display.Text("${stringWithPlaceholder(lineBuffer[4])}", 10, 80, display.GREEN);`;
-    replCommmand += `t3 = display.Text("${stringWithPlaceholder(lineBuffer[3])}", 10, 150, display.GREEN);`;
-    replCommmand += `t4 = display.Text("${stringWithPlaceholder(lineBuffer[2])}", 10, 220, display.GREEN);`;
-    replCommmand += `t5 = display.Text("${stringWithPlaceholder(lineBuffer[1])}", 10, 290, display.GREEN);`;
-    replCommmand += `t6 = display.Text("${stringWithPlaceholder(lineBuffer[0])}", 10, 360, display.GREEN);`;
+    replCommmand += `t1 = display.Text("${stringWithPlaceholder(lineBuffer[5])}", 10, 10, display.WHITE);`;
+    replCommmand += `t2 = display.Text("${stringWithPlaceholder(lineBuffer[4])}", 10, 80, display.WHITE);`;
+    replCommmand += `t3 = display.Text("${stringWithPlaceholder(lineBuffer[3])}", 10, 150, display.WHITE);`;
+    replCommmand += `t4 = display.Text("${stringWithPlaceholder(lineBuffer[2])}", 10, 220, display.WHITE);`;
+    replCommmand += `t5 = display.Text("${stringWithPlaceholder(lineBuffer[1])}", 10, 290, display.WHITE);`;
+    replCommmand += `t6 = display.Text("${stringWithPlaceholder(lineBuffer[0])}", 10, 360, display.WHITE);`;
     replCommmand += `display.show(t1, t2, t3, t4, t5, t6);`;
     replSend(replCommmand + enter);
   }
@@ -56,7 +54,6 @@ console.log("Connected");
 replSend("import device; device.battery_level();" + enter);
 replSend("import led; led.off(led.RED);" + enter);
 // replSend("import display; display.brightness(4);" + enter);
-replSend('\x05' + displayAPI + '\x04');
 
 setInterval(sendLastLine, 500);
 setInterval(inactivityCheck, 5000);
